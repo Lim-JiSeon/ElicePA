e0e5908a301cc70632d69310d2c88a0d21643723
Lim_JiSeon <dpf226@naver.com>
2024-02-12 05:09:32 +0900

    feat: 초기환경 설정

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const CourseSearch = () => {
+  return (
+    <>
+      <Head>
+        <title>Course Search</title>
+      </Head>
+      <main className={`${inter.className}`}></main>
+    </>
+  );
+};
+
+export default CourseSearch;
