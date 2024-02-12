4f624f2589ab4e5c5bf49682ca6685256d056cb9
Lim_JiSeon <dpf226@naver.com>
2024-02-12 16:03:30 +0900

    fix(Chip.tsx): click 로직 수정

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,30 +1,35 @@
 import SessionStorage from "@/utils/SessionStorage";
-import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
 type ChipProps = {
   text: string;
-  name: string;
-  queryKey: string;
-  value: number | boolean;
+  name: number;
 };
 
-export const Chip = ({ text, name, queryKey, value }: ChipProps) => {
-  const initChecked = SessionStorage.getItem("chip-value")?.name;
-  const [checked, setChecked] = useState(initChecked);
+export const Chip = ({ text, name }: ChipProps) => {
+  const [checked, setChecked] = useState(false);
 
   const handleClick = () => {
     setChecked(!checked);
 
-    const chipValue = checked ? "" : `${queryKey}=${value}`;
-    SessionStorage.setItem("chip-value", `{ ${name}: ${chipValue} }`);
+    const array = SessionStorage.getItem("chip-value") ?? [];
+
+    const newArray = checked
+      ? array.filter((element: number) => element !== name)
+      : [...array, name];
+
+    SessionStorage.setItem("chip-value", newArray);
   };
+
+  useEffect(() => {
+    const initChecked = SessionStorage.getItem("chip-value")?.includes(name);
+    setChecked(initChecked);
+  }, [name]);
 
   return (
     <button
       key={name}
       className={`chip-button ${checked ? "checked" : ""}`}
-      data-checked={checked}
       onClick={handleClick}>
       <span>{text}</span>
 
@@ -39,16 +44,16 @@ export const Chip = ({ text, name, queryKey, value }: ChipProps) => {
           cursor: pointer;
         }
 
+        .checked {
+          background-color: #524fa1;
+          color: #ffffff;
+        }
+
         button:hover {
           background-image: linear-gradient(
             rgba(0, 0, 0, 0.2),
             rgba(0, 0, 0, 0.2)
           );
-        }
-
-        .checked {
-          background-color: #524fa1;
-          color: #ffffff;
         }
       `}</style>
     </button>
